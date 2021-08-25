14efe6e5bbbdc39ec53a5920936557d026e56347
Vehbi Hamza <vehbihamza@outlook.com>
2021-08-25 18:51:53 +0200

    Added some C# Projects

---
+++ LearningGitCSProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningGitCSProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning Git");
+        }
+    }
+}
